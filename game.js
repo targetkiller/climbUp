21a1cb78d55908c10858862f851c84984dcfc1ad
targetkiller <369160020@qq.com>
2014-05-13 22:33:05 +0800

    游戏基本完成
    1.增加加速；2.增加反向操作；3.增加陀螺仪（注释掉）

--- game.js
+++ game.js
@@ -28,33 +28,67 @@ window.RAF =
    )
 ();
 
-
 // 键盘控制响应.......................................................
 window.onkeydown = function (e) {
    var key = e.keyCode;
-
    if (key === 80) {  // 'p'
       // 暂停
    }
-
    if (key === 37) { // left arrow
-      climbUp.roleX = climbUp.toRight;
-   }
-
-   else if (key === 39) { // right arrow
       climbUp.roleX = climbUp.toLeft;
+   }
+   else if (key === 39) { // right arrow
+      climbUp.roleX = climbUp.toRight;
    }
 };
 
 // 鼠标或触摸响应.......................................................
 document.addEventListener('touchstart',function(ev){
-	if(ev.touches[0].pageX>climbUp.Bwidth/2){
+	if(ev.touches[0].pageX<climbUp.Bwidth/2){
       climbUp.roleX = climbUp.toLeft;
 	}
 	else{  
       climbUp.roleX = climbUp.toRight;
 	}
-},false);	
+},false);
+
+// 重力感应
+// function Orientation(selector) {
+	          
+// }
+
+// Orientation.prototype.init = function(){
+//     window.addEventListener('deviceorientation', this.orientationListener, false);
+// }
+
+// Orientation.prototype.orientationListener = function(evt) {
+//   var gamma = evt.gamma
+//   var beta = evt.beta
+//   var alpha = evt.alpha
+
+//   if(climbUp.changeSideDir==0){
+// 	if(gamma<0){
+// 	  	climbUp.roleX = climbUp.toLeft;
+// 	}
+// 	else{
+// 	  	climbUp.roleX = climbUp.toRight;
+// 	}
+//   }
+//   else{
+//   	if(gamma>0){
+// 	  	climbUp.roleX = climbUp.toLeft;
+// 	}
+// 	else{
+// 	  	climbUp.roleX = climbUp.toRight;
+// 	}	
+//   }
+// }
+
+// 重新开始游戏.......................................................
+var restart = document.getElementById('restart');
+restart.addEventListener('click',function(ev){
+	climbUp.reset();
+},false);
 
 var ClimbUp = function(){
 	// init ---------------------------
@@ -66,6 +100,11 @@ var ClimbUp = function(){
 	this.canvas.height = this.Bheight,
 
 	// setting ------------------------
+	// this.coinSound = document.getElementById('coin-sound'),
+	// this.audioTracks = [new Audio()],
+	// this.COIN_VOLUME = 1.0,
+	// this.coinSound.volume = this.COIN_VOLUME,
+
 	this.treeWidth = 40,
 	this.treeHeight = this.Bheight,
 	this.treeColor = "#770000",
@@ -74,20 +113,32 @@ var ClimbUp = function(){
 	this.branchHeight = 30,
 	this.branchColor = "#770000",
 	this.branchs = [],
-	this.branchsVelocity = 15,
+	this.branchsSpeed = 5,
+	this.branchsVelocity = 0.005,
+	this.branchsNum = 1,
 
 	this.roleWidth = 40,
 	this.roleHeight = 40,
-	this.roleColor = "#007700",
+	this.roleColor = "#ffff00",
 	this.toLeft = (this.Bwidth-this.treeWidth)/2-this.roleWidth,
 	this.toRight = (this.Bwidth+this.treeWidth)/2,
 	this.roleX = this.toLeft;
 	this.roleY = this.Bheight-50;
 
+	this.score = 0;
+	this.scoreVelocity = 1;
+
+	this.startTime = +new Date(),
+	this.changeSideBaseTime = 5000,
+	this.changeSideRanTime = 10000,
+	this.changeSideTime = Math.floor(Math.random()*this.changeSideBaseTime)+this.changeSideRanTime,
+	this.changeSideDir = 0;//0:正,1:反
+
+	this.stopGame = false;
 	// control ------------------------
 	this.branchsInit = {
 		execute:function(){
-			for(var i = 0; i < 1; i++){
+			for(var i = 0; i < climbUp.branchsNum; i++){
 				this.branchInit();
 			}
 		},
@@ -114,6 +165,16 @@ ClimbUp.prototype = {
 		this.drawAll();
 		RAF(function(){climbUp.animate();});
 	},
+	reset:function(){
+		this.branchsVelocity = 0.01;
+		this.branchsSpeed = 5;
+		this.score = 0;
+		this.scoreVelocity = 1;
+		this.branchs=[];
+		this.branchsInit.execute();
+		document.getElementById('gameover').style.display="none";
+		this.stopGame = false;
+	},
 	drawAll:function(){
 		this.drawTree();
 		this.drawBranch();
@@ -121,8 +182,23 @@ ClimbUp.prototype = {
 		if(this.isCollide()){
 			// 发生碰撞
 			document.getElementById('gameover').style.display="block";
+			document.getElementById('finalScore').innerText=this.score;
+			this.branchsVelocity = 0;
+			this.branchsSpeed = 0;
+			this.scoreVelocity = 0;
 			this.branchs = [];
+			this.stopGame = true;
 		}
+		if(!this.stopGame){
+			if(this.isNeedChangeSide()){
+				// console.log("changesize!");
+				this.changeSide();
+			}
+		}
+		// 游戏加速
+		this.branchsSpeed += this.branchsVelocity;
+		//document.getElementById('speed').innerText=this.branchsSpeed.toFixed(2);
+		
 	},
 	drawTree:function(){
 		this.ctx.save();
@@ -135,10 +211,15 @@ ClimbUp.prototype = {
 		this.ctx.fillStyle = this.branchColor;
 		for(var i = 0; i < this.branchs.length; i++){
 			// 枝叶的y值每次减去相应距离
-			this.branchs[i].y += this.branchsVelocity;
+			this.branchs[i].y += this.branchsSpeed;
 			this.ctx.fillRect(this.branchs[i].x,this.branchs[i].y,this.branchWidth,this.branchHeight);
 			// 枝叶超出高度新建枝叶
 			if(this.branchs[i].y>this.Bheight){
+				// 游戏增分
+				this.score += this.scoreVelocity;
+				document.getElementById('score').innerText=this.score;
+				// climbUp.coinSound.play();
+				document.getElementById('coin-sound').play();
 				this.branchs=[];
 				this.branchsInit.execute();
 			}
@@ -176,8 +257,57 @@ ClimbUp.prototype = {
                 this.ctx.isPointInPath(centerX, centerY) ||
                 this.ctx.isPointInPath(left,    bottom)  ||
                 this.ctx.isPointInPath(right,   bottom);
+	},
+	// 判断转向的发生时机
+	isNeedChangeSide:function(){
+		var now = +new Date();
+		return (now-this.startTime)>this.changeSideTime;
+	},
+	// 操作转向
+	changeSide:function(){
+		var left,right;
+		// 转向，0是正，1是反，默认是正
+		if(this.changeSideDir==1){
+			left = climbUp.toLeft;
+			right = climbUp.toRight;
+			document.getElementsByTagName('body')[0].style.backgroundColor = "#333";
+			this.changeSideDir = 0;
+		}
+		else{
+			left = climbUp.toRight;
+			right = climbUp.toLeft;
+			document.getElementsByTagName('body')[0].style.backgroundColor = "#aaa";
+			this.changeSideDir = 1;
+		}
+
+		// 键盘控制响应.......................................................
+		window.onkeydown = function (e) {
+		   var key = e.keyCode;
+		   if (key === 37) { // left arrow
+		      climbUp.roleX = left;
+		   }
+		   else if (key === 39) { // right arrow
+		      climbUp.roleX = right;
+		   }
+		};
+
+		// 鼠标或触摸响应.......................................................
+		document.addEventListener('touchstart',function(ev){
+			if(ev.touches[0].pageX<climbUp.Bwidth/2){
+		      climbUp.roleX = left;
+			}
+			else{  
+		      climbUp.roleX = right;
+			}
+		},false);
+
+		// 重新设置下一次转向时间
+		this.startTime = +new Date();
+		this.changeSideTime = Math.floor(Math.random()*this.changeSideBaseTime)+this.changeSideRanTime;
 	}
 }
 
 var climbUp = new ClimbUp();
 climbUp.start();
+// (new Orientation()).init();
+
